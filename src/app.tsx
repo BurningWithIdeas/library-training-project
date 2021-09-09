3a8c8e9b54e8eb29fce605d63c17a1dd72a35005
Andrey Karev <zirockz@mail.ru>
2021-09-09 10:52:57 +0300

    fixed form refreses on request bug

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import SearchForm from 'Common/search-form';
 import SearchPage from './pages/search-page/search-page';
 import ItemPage from './pages/item-page/item-page';
 
@@ -9,6 +10,8 @@ function App() {
       <Route exact path="/">
         <Redirect to="/search" />
       </Route>
+
+      <SearchForm />
 
       <Route exact path="/search" component={SearchPage} />
       <Route path="/search/:item" component={ItemPage} />
